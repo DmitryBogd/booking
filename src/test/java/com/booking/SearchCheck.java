6d9897c74feb20c3064738dc7f315c5dfb584f1e
DmitryBogd <1999dima90@gmail.com>
2021-08-18 01:18:09 +0300

    Created BasicPage.java for test search on basic page.

--- src/test/java/com/booking/SearchCheck.java
+++ src/test/java/com/booking/SearchCheck.java
@@ -1,9 +1,12 @@
 package com.booking;
 
 
+import Constants.Month;
 import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.BasicPage;
 
 public class SearchCheck {
     private static ChromeDriver driver;
@@ -13,19 +16,23 @@ public class SearchCheck {
         System.setProperty("webdriver.chrome.driver", "F:/ForJava/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.booking.com/index.en-gb.html");
+        System.out.println("test start");
     }
 
     @AfterClass
     public static void afterTests(){
         driver.quit();
+        System.out.println("test closed");
     }
 
-
     @Test
-    public void openBooking(){
+    public void searchHotelInCity(){
+        BasicPage basicPage = new BasicPage(driver);
 
-        String title = driver.getTitle();
-        Assert.assertTrue(title.equals("Booking.com | Official site | The best hotels & accommodation"));
+        basicPage.enterCityName("NewYork");
+        basicPage.enterCheckInAndCheckOutDate(Month.April, 19,Month.May, 18, 2022);
+        basicPage.clickSearch();
+
 
     }
 
--- src/test/java/pages/BasicPage.java
+++ src/test/java/pages/BasicPage.java
@@ -1,11 +1,43 @@
 package pages;
 
+import Constants.Month;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class BasicPage {
+    private  WebDriver driver;
+
+    public BasicPage(WebDriver driver){
+        this.driver = driver;
+    }
+
     public void enterCityName(String city){
+        driver.findElement(By.id("ss")).sendKeys(city);
+        System.out.println("We enter city: "+ city);
+    }
+
+    public void enterCheckInAndCheckOutDate(Month checkInMonth, int chekInDay, Month checkOutMonth, int chekOutDay, int year){
+        driver.findElement(By.cssSelector("div[class=\"xp__dates xp__group\"]")).click();
+        findMonthAndDayThenClick(checkInMonth,chekInDay,year);
+        findMonthAndDayThenClick(checkOutMonth,chekInDay,year);
+    }
+
+    private void findMonthAndDayThenClick(Month month, int day, int year){
+
+        String name = driver.findElement(By.cssSelector("div[class=\"bui-calendar__month\"]")).getText();
+
+        while (!name.equals(month + " " + year)) {
+            driver.findElement(By.cssSelector("div[class=\"bui-calendar__control bui-calendar__control--next\"]")).click();
+            name = driver.findElement(By.cssSelector("div[class=\"bui-calendar__month\"]")).getText();
+        }
+        driver.findElement(By.xpath("//span[.='"+ day +"']")).click();
+        System.out.println("we have chosen a date: "+ day + " " + month + " " + year);
 
     }
 
-    public void enterCheckInAndCheckOutDate(int chekIn, int chekOut){
+    public void clickSearch(){
+        driver.findElement(By.cssSelector("button[class=\"sb-searchbox__button \"]")).click();
 
     }
 }
